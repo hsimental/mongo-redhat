167a70fd3ce7682199ead7fe08b49e948b71f689
hsimental <hsimental@compusoluciones.com>
2024-08-23 19:02:43 -0600

    Add files via upload

--- app1.js
+++ app1.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
  
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8081;
  
 let appStatus = 'Aplicación en proceso de inicialización';
  
